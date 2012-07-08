dcbe57e5ec33c65eff23dfb4fc46480fa968e332
Tobias Kortkamp <tobias.kortkamp@gmail.com>
2012-07-08 16:25:26 +0200

    Add web service

--- web/web.go
+++ web/web.go
@@ -3,23 +3,93 @@ package web
 import (
 	"fmt"
 	"log"
+	"path"
 	"net"
+	"time"
 	"net/http"
+	"html/template"
 
-	"voicemail/utils"
+	sqlite "github.com/gwenn/gosqlite"
+
+	. "voicemail/utils"
+
+	"voicemail/web/assets"
 )
 
+var db *sqlite.Conn
+var rootTemplate *template.Template
+var voicemailDir string
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	s, err := db.Prepare("SELECT * from voicemail ORDER BY date DESC")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return;
+	}
+	defer s.Finalize()
+
+	calls := []Call{}
+	err = s.Select(func(s *sqlite.Stmt) (err error) {
+		var call Call
+		var duration string
+		var voicemailPath string
+		if err = s.NamedScan(
+			"id", &call.Id,
+			"caller", &call.Caller,
+			"called", &call.Called,
+			"date", &call.Date,
+			"duration", &duration,
+			"voicemail", &voicemailPath); err != nil {
+			return err
+		}
+
+		call.VoicemailPath = path.Join("/voicemail", voicemailPath)
+		call.Duration, _ = time.ParseDuration(duration + "s")
+		calls = append(calls, call)
+
+		return err
+	})
+	
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return
+	}
+
+	err = rootTemplate.ExecuteTemplate(w, "calls", calls)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return;
+	}
+}
+
+func handleAsset(f func() []byte, t string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", t)
+		w.Write(f())
+	}
+}
+
 func Serve(l net.Listener, dbFile string, voicemailDir string) {
-	db, _ := utils.OpenDatabase(dbFile)
+	db, _ = OpenDatabase(dbFile)
 	defer db.Close()
 
-	http.Handle("/voicemail/",
-		http.StripPrefix("/voicemail/",
+	rootTemplate = template.New("root")
+	_, err := rootTemplate.Parse(string(app_html()))
+	if err != nil {
+		panic(err)
+	}
+	
+	http.Handle("/voicemail/", http.StripPrefix("/voicemail/",
 		http.FileServer(http.Dir(voicemailDir))))
 
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
-	})
+	http.HandleFunc("/js", handleAsset(assets.Jquery_min_js, "text/javascript"))
+	http.HandleFunc("/css", handleAsset(assets.Bootstrap_combined_min_css, "text/css"))
+	http.HandleFunc("/img/glyphicons-halflings.png",
+		handleAsset(assets.Glyphicons_halflings_png, "image/png"))
+	http.HandleFunc("/img/glyphicons-halflings-white.png",
+		handleAsset(assets.Glyphicons_halflings_white_png, "image/png"))
+	
+	http.HandleFunc("/", rootHandler)
 
 	log.Fatal(http.Serve(l, nil))
 }
